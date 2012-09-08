23bc6f4a5084db0a1c0537ad8fb812a63e7acfb2
Matan Nassau <matan.nassau@gmail.com>
2012-09-08 17:39:39 -0400

    simplify, clarify usage message

--- src/main.cc
+++ src/main.cc
@@ -7,7 +7,15 @@ int main(int argc, char const * argv[])
 {
     acp::cmdline conf(argc, argv);
     if( ! conf.ok() ) {
-        std::cout << " usage: " << argv[0] << " src0 [src1 [src2 [...]]] tgt\n";
+        std::cout << " usage: " << argv[0] << " src0 [src1 [...]] tgt\n"
+                     "\n"
+                     "src0, ... are file or directory names.\n"
+                     "tgt       is a directory name.\n"
+                     "\n"
+                     "copy the given source files and directories into the\n"
+                     "target directory. this is an all-or-nothing operation:\n"
+                     "either all files successfully copy or none at all\n"
+                     "copy.\n";
         return 0;
     }
     auto * out(conf.v ? &std::cout : nullptr);
