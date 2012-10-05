03eaf10abd9d3685726146fe266b82d4a57ff834
Matan Nassau <matan.nassau@gmail.com>
2012-10-04 23:13:09 -0400

    test copying a file with cp_file

--- test/main.cc
+++ test/main.cc
@@ -1,7 +1,18 @@
 #define CATCH_CONFIG_MAIN
 #include <catch/catch.hpp>
+#include "sandbox.hh"
+#include <acp/cp_file.hh>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <fstream>
 
-// TEST_CASE( "group/name", "details" ) {
-//     auto const sum(1 + 2);
-//     REQUIRE( sum == 3 );
-// }
+namespace fs = boost::filesystem;
+
+TEST_CASE( "cp_file/copy a file", "" ) {
+    acp::test::sandbox box;
+    auto const source(box.path() / "file");
+    std::ofstream(source.string());
+    fs::path const target(source.string() + ".cp");
+    acp::cp_file op(source, target);
+    REQUIRE( fs::is_regular_file(target) );
+}
