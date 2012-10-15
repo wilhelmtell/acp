e1546544c016469e3a3ed26eaf0d3b7d78987862
Matan Nassau <matan.nassau@gmail.com>
2012-10-14 23:45:37 -0400

    test verify(), not detail::verify_*()

--- test/verify.cc
+++ test/verify.cc
@@ -1,5 +1,6 @@
 #include <catch/catch.hpp>
 #include "variables_map.hh"
+#include "directory.hh"
 #include <acp/verify.hh>
 #include <acp/verify_error.hh>
 #include <vector>
@@ -7,29 +8,33 @@
 
 namespace test = acp::test;
 
-TEST_CASE( "verify/source files/all exist", "" ) {
-    test::variables_map vm;
-    vm["source_files"] = std::vector<std::string>({ "file_0" });
-    REQUIRE_THROWS_AS(
-        acp::detail::verify_source_files(vm),
-        acp::source_files_not_found
-    );
-}
-
-TEST_CASE( "verify/source files/not empty", "" ) {
-    test::variables_map vm;
-    REQUIRE_THROWS_AS(
-        acp::detail::verify_source_files(vm),
-        acp::source_files_count
-    );
-}
-
 TEST_CASE( "verify/target directory/multiple targets throw", "" ) {
     test::variables_map vm;
     vm["target_directory"] = "directory_0";
     vm["target_file"] = "file_0";
     REQUIRE_THROWS_AS(
-        acp::detail::verify_target(vm),
+        acp::verify(vm),
         acp::target_count
+    );
+}
+
+TEST_CASE( "verify/source files/not empty", "" ) {
+    test::variables_map vm;
+    test::directory directory;
+    vm["target_directory"] = directory.path().string();
+    REQUIRE_THROWS_AS(
+        acp::verify(vm),
+        acp::source_files_count
+    );
+}
+
+TEST_CASE( "verify/source files/all exist", "" ) {
+    test::variables_map vm;
+    test::directory directory;
+    vm["target_directory"] = directory.path().string();
+    vm["source_files"] = std::vector<std::string>({ "file_0" });
+    REQUIRE_THROWS_AS(
+        acp::verify(vm),
+        acp::source_files_not_found
     );
 }
